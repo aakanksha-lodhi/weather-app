ecba71e51133f4b40c0f3ebd77fc8ba6024e0b17
aakanksha-lodhi <aakanksha.lodhi@adypu.edu.in>
2021-10-27 09:53:33 +0530

    project

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"6f3eb77d87d6a0dd77964cd4e6d45034"
+    }
+}
+
+module.exports = constants;
